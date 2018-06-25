432518ac7fcdbe7e9207c0ca0022ff478d76263c
voyage4301 <815717711@qq.com>
2018-06-25 13:26:24 +0800

    登录功能完成

---
+++ public/back/js/login.js
@@ -0,0 +1,35 @@
+$(function () {
+
+  ; //登陆验证
+  (function () {
+    $('#form').bootstrapValidator({
+      fields: {
+        username: {
+          validators: {
+            notEmpty: {
+              message: '用户名不能为空!'
+            },
+            stringLength: {
+              min: 2,
+              max: 6,
+              message: '用户名长度为2-6位!'
+            }
+          }
+        },
+        password: {
+          validators: {
+            notEmpty: {
+              message: '密码不能为空!'
+            },
+            stringLength: {
+              min: 6,
+              max: 12,
+              message: '密码长度为6-12位!'
+            }
+          }
+        }
+      }
+    })
+  })();
+
+})
