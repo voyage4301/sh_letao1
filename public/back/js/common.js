ad6a8cdf99bc33c8d17b03f8ec7f2c01cfdab20b
voyage4301 <815717711@qq.com>
2018-06-26 22:03:22 +0800

    公共js开始

---
+++ public/back/js/common.js
@@ -0,0 +1,33 @@
+$(function () {
+
+  ; //全局进度条事件
+  (function () {
+    $(document).ajaxStart(function () {
+      NProgress.start()
+    })
+    $(document).ajaxStop(function () {
+      setTimeout(() => {
+        NProgress.done()
+      }, 500);
+    })
+  })();
+
+
+
+  ; //全局功能
+  (function () {
+    //1.页面拦截
+    (location.href.indexOf('login.html') === -1) && $.ajax({
+      type: 'get',
+      url: '/employee/checkRootLogin',
+      dataType: 'json',
+      success: function (info) {
+        console.log(info);
+        (info.error === 400) && (location.href = './login.html')
+      }
+    })
+
+    
+  })();
+
+})
