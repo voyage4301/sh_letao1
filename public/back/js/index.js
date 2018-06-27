0689633dea65000474d1578c05a689b5eba47c5e
voyage4301 <815717711@qq.com>
2018-06-27 14:57:19 +0800

    主页完成,用户开始

--- public/back/js/index.js
+++ public/back/js/index.js
@@ -26,46 +26,59 @@ $(function () {
     };
 
     option1 = {
-      title : {
-          text: '热门品牌销售',
-          subtext: '2018年6月',
-          x:'center'
+      title: {
+        text: '热门品牌销售',
+        subtext: '2018年6月',
+        x: 'center'
       },
-      tooltip : {
-          trigger: 'item',
-          formatter: "{a} <br/>{b} : {c} ({d}%)"
+      tooltip: {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
       },
       legend: {
-          orient: 'vertical',
-          left: 'left',
-          data: ['耐克','阿迪','李宁','新百伦','阿迪王']
+        orient: 'vertical',
+        left: 'left',
+        data: ['耐克', '阿迪', '李宁', '新百伦', '阿迪王']
       },
-      series : [
+      series: [{
+        name: '访问来源',
+        type: 'pie',
+        radius: '55%',
+        center: ['50%', '60%'],
+        data: [{
+            value: 335,
+            name: '耐克'
+          },
           {
-              name: '访问来源',
-              type: 'pie',
-              radius : '55%',
-              center: ['50%', '60%'],
-              data:[
-                {value:335, name:'耐克'},
-                {value:310, name:'阿迪'},
-                {value:234, name:'李宁'},
-                {value:135, name:'新百伦'},
-                {value:1548, name:'阿迪王'}
-              ],
-              itemStyle: {
-                  emphasis: {
-                      shadowBlur: 10,
-                      shadowOffsetX: 0,
-                      shadowColor: 'rgba(0, 0, 0, 0.5)'
-                  }
-              }
+            value: 310,
+            name: '阿迪'
+          },
+          {
+            value: 234,
+            name: '李宁'
+          },
+          {
+            value: 135,
+            name: '新百伦'
+          },
+          {
+            value: 1548,
+            name: '阿迪王'
           }
-      ]
-  };
-  
+        ],
+        itemStyle: {
+          emphasis: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }]
+    };
+
     // 使用刚指定的配置项和数据显示图表。
     bar.setOption(option);
     pie.setOption(option1)
   })();
+  
 })
